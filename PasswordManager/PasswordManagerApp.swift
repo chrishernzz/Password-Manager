d7c12b4216501a6387b237a49c2996d90d710e53
Christian Hernandez <christianhernandez1014@gmail.com>
2024-09-14 13:15:48 -0700

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Daniel Hernandez on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
